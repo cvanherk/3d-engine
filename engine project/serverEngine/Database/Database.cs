d3039ebb5e6afe68772585106cabeb21ad595548
Corné van Herk <corne@hybridsaas.com>
2016-02-01 12:33:32 +0100

    Database class toegevoegd

---
+++ engine project/serverEngine/Database/Database.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+
+namespace serverEngine.Database
+{
+    public class Database
+    {
+        public static Database Instance => new Database();
+
+
+        private readonly string _connectionString;
+        public Database(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public Database()
+        {
+            _connectionString = File.ReadAllText("connectionstring.setting");
+        }
+
+        public T ExecuteScalar<T>(string queryString, params SqlParameter[] parameters)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var command = new SqlCommand(queryString, connection);
+                command.Parameters.AddRange(parameters);
+                command.Connection.Open();
+                return (T)command.ExecuteScalar();
+            }
+        }
+
+        /// <summary>
+        /// Voert een SQL commando uit en haalt het resultaat op
+        /// </summary>
+        /// <param name="queryString"></param>
+        /// <param name="parameters"></param>
+        /// <returns>Returnd een lijst met resultaten uit database</returns>
+        public IEnumerable<Dictionary<string, object>> Query(string queryString, params SqlParameter[] parameters)
+        {
+            var results = new DataTable();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand(queryString, connection))
+                {
+                    command.Parameters.AddRange(parameters);
+
+                    using (var dataAdapter = new SqlDataAdapter(command))
+                        dataAdapter.Fill(results);
+
+                    foreach (DataRow row in results.Rows)
+                    {
+                        var items = new Dictionary<string, object>();
+                        foreach (var column in row.Table.Columns.Cast<DataColumn>())
+                        {
+                            items[column.ColumnName] = row[column.ColumnName];
+                        }
+
+                        yield return items;
+
+                    }
+                }
+            }
+
+
+        }
+
+        public int ExecuteNonQuery(string queryString, params SqlParameter[] parameters)
+        {
+            using (var connection = new SqlConnection(
+               _connectionString))
+            {
+                var command = new SqlCommand(queryString, connection);
+                command.Parameters.AddRange(parameters);
+                command.Connection.Open();
+                return command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ engine project/serverEngine/Database/SqlHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serverEngine.Database
+{
+    public static class SqlHelper
+    {
+        public static SqlParameter ToSqlParameter<T>(this T objectToSql, string name, params object[] parameters)
+        {
+            if (parameters.Length > 0)
+                name = String.Format(name, parameters);
+            // ReSharper disable once CompareNonConstrainedGenericWithNull
+            return objectToSql == null ? new SqlParameter(name, DBNull.Value) : new SqlParameter(name, objectToSql);
+        }
+
+        public static SqlParameter ToSqlParameter<T>(this T? objectToSql, string name, params object[] parameters) where T : struct
+        {
+            if (parameters.Length > 0)
+                name = String.Format(name, parameters);
+            return !objectToSql.HasValue ? new SqlParameter(name, DBNull.Value) : new SqlParameter(name, objectToSql.Value);
+        }
+    }
+}

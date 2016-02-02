68ccb3bc5fcd2c5a67956b2ba0f213d4e3e0226a
Robin Tempert <robintempert@hotmail.com>
2016-02-02 14:00:49 +0100

    Game objecten en positie

---
+++ engine project/ClientEngine/Test/TestGameObject.cs
@@ -0,0 +1,18 @@
+﻿using ClientEngine.Objecten;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientEngine.Test
+{
+    class TestGameObject : GameObject
+    {
+        public TestGameObject(float z, float x)
+        {
+            Position.Z = z;
+            Position.X = x;
+        }
+    }
+}

2bd9b9487369d2df7779c0deb76b404aee2c4257
thomask22 <nlsilverbolts@gmail.com>
2016-02-04 09:41:20 +0100

    camera fix en input prototype

--- engine project/ClientEngine/Test/TestGameObject.cs
+++ engine project/ClientEngine/Test/TestGameObject.cs
@@ -13,7 +13,7 @@ namespace ClientEngine.Test
     {
         public TestGameObject()
         {
-            Position.Z = -7f;
+            Position.Z = -10f;
         } 
 
         public override void Start()
@@ -22,14 +22,79 @@ namespace ClientEngine.Test
             Mesh = objLoader.CreateMeshStruct(@"cube.obj");
         }
 
+        private float OnXAccelerate()
+        {
+            xAcceleration += 0.1f;
+            return xAcceleration;
+        }
+
+        private void ClearXAcceleration()
+        {
+            xAcceleration = 0;
+        }
+
+        private float OnZAccelerate()
+        {
+            zAcceleration += 0.1f;
+            return zAcceleration;
+        }
+
+        private void ClearZAcceleration()
+        {
+            zAcceleration = 0;
+        }
+
         public void OnKeyDown(object sender, KeyEventArgs keyEventArgs)
         {
             Console.WriteLine($"OnkeyDown: {keyEventArgs.KeyCode}");
+
+            switch(keyEventArgs.KeyCode)
+            {
+                case Keys.W:
+                    Position.X += OnXAccelerate();
+                break;
+
+                case Keys.S:
+                    Position.X -= OnXAccelerate();
+                break;
+
+                case Keys.D:
+                    Position.Z += OnZAccelerate();
+                    break;
+
+                case Keys.A:
+                    Position.Z -= OnZAccelerate();
+                    break;
+
+                case Keys.P:
+                    Position.X = 0;
+                    Position.Z = 0;
+                    break;
+            }
         }
 
         public void OnKeyUp(object sender, KeyEventArgs keyEventArgs)
         {
             Console.WriteLine($"Onkeyup: {keyEventArgs.KeyCode}");
+
+            switch (keyEventArgs.KeyCode)
+            {
+                case Keys.W:
+                    ClearXAcceleration();
+                break;
+
+                case Keys.S:
+                    ClearXAcceleration();
+               break;
+
+                case Keys.D:
+                    ClearZAcceleration();
+                    break;
+
+                case Keys.A:
+                    ClearZAcceleration();
+                    break;
+            }
         }
     }
 }
